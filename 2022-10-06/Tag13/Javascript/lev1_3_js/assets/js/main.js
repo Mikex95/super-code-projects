44b52b846bf8c116470f98a0865833cc24cb88f4
Mikex95 <mike.beha1337@gmail.com>
2022-10-08 13:44:23 +0200

    headphone-website

--- 2022-10-06/Tag13/Javascript/lev1_3_js/assets/js/main.js
+++ 2022-10-06/Tag13/Javascript/lev1_3_js/assets/js/main.js
@@ -4,3 +4,4 @@ gallery.innerHTML = "<figure><img src='https://images.unsplash.com/photo-1664993
 gallery.innerHTML += "<figure><img src='https://images.unsplash.com/photo-1664993981856-2076348ed8f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60'> <figcaption>An elephant at sunset</figcaption></figure>"
 
 gallery.innerHTML += "<figure><img src='https://images.unsplash.com/photo-1664993981856-2076348ed8f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60'><figcaption>An elephant at sunset</figcaption></figure>"
+
